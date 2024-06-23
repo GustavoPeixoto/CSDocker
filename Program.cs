5eef9bc05788d0bb8d78a98e9ee1c93105629ff0
GustavoPeixoto <gustavopcamargos@gmail.com>
2024-06-23 19:32:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Globalization;
+
+namespace CsDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
